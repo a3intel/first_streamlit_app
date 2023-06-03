31168643651d1c30d0d96f000119bc70190f2425
a3intel <135195145+a3intel@users.noreply.github.com>
2023-06-03 10:05:08 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,5 @@ streamlit.title(' 🥣 My Parents new healthy diner')
 streamlit.header('Breakfast menu')
 streamlit.text('Sandwich')
 streamlit.text('Juice')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
